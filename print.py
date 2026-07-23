dec741419a27394558e550fabf3188f7face1c98
stanmqz <moscarstanley37@gmail.com>
2026-07-23 11:58:19 -0600

    clase

---
+++ print.py
@@ -0,0 +1,2 @@
+print("Hola mi nombre es \"Stn\"")
+print('Hola soy stanley \nSoy estudiante de la materia DS')
---
+++ tipos_dato.py
@@ -0,0 +1,33 @@
+import sys
+# print('----Numeros Enteros------')
+# print(f'Tamaño de un entero : ${sys.getsizeof(0)} bytes')
+# numero = 10**100
+# print(numero)
+
+
+#Numeros deciamles (float)
+# print(f'Tamaño de un entero : ${sys.getsizeof(0)} bytes')
+# print(f'Tamaño de un entero : ${sys.float_info.max}')
+
+# #Valor booleano
+# print(True)
+# print(False)
+# print(type(False))
+
+# print(int(True))
+
+
+
+print(f'Tamaño de un entero : ${sys.getsizeof(0)} bytes')
+# print(f'Tamaño de un entero : ${sys.float_info.max}')
+
+# #Valor booleano
+# print(True)
+# print(False)
+# print(type(False))
+
+# print(int(True))
+
+
+
+

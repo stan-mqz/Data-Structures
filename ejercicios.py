ac7740115b59afe2eece981bc44337c9324fd398
stanmqz <moscarstanley37@gmail.com>
2026-07-24 15:51:28 -0600

    Add comments to clarify code functionality in ejercicios.py

--- ejercicios.py
+++ ejercicios.py
@@ -1,4 +1,6 @@
 # Ejercicio 1: Imprimir texto multilínea
+
+# Usamos comillas triples (""") para respetar los saltos de línea sin errores de sintaxis.
 print("""If we could see tomorrow, what are your plans? No one can live in sorrow, ask all
 your friends. Times that you took in stride they're back in demand. I was the one who's
 washing blood off your hands""")
@@ -6,9 +8,12 @@ washing blood off your hands""")
 
 # Ejercicio 2: Crear una matriz 3x3 de ceros
 
+# Usamos listas anidadas
 matriz_ceros = [[0 for _ in range(3)] for _ in range(3)]
 print(matriz_ceros)
 
+
+# En Python, el símbolo \ es un carácter de escape, por lo que se debe escribir doble (\\) para que se imprima correctamente.
 
 # Ejercicio 3: Imprimir los símbolos \ y /
 print("\\ y /")

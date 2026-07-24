b62bb9e78f1970f23e3e578e63dbba1f9a8c3791
stanmqz <moscarstanley37@gmail.com>
2026-07-24 15:31:19 -0600

    ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,14 @@
+# Ejercicio 1: Imprimir texto multilínea
+print("""If we could see tomorrow, what are your plans? No one can live in sorrow, ask all
+your friends. Times that you took in stride they're back in demand. I was the one who's
+washing blood off your hands""")
+
+
+# Ejercicio 2: Crear una matriz 3x3 de ceros
+
+matriz_ceros = [[0 for _ in range(3)] for _ in range(3)]
+print(matriz_ceros)
+
+
+# Ejercicio 3: Imprimir los símbolos \ y /
+print("\\ y /")

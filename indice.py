ff88d984d49f1f4499088e24a826d1b62f3825bb
stanmqz <moscarstanley37@gmail.com>
2026-08-22 14:52:20 -0600

    listas

---
+++ indice.py
@@ -0,0 +1,14 @@
+nombre = 'chepito'
+
+print(nombre)
+print(nombre[-1])
+
+
+usuario = 'chepito'
+nuevo_usuario = "C" + usuario[1:] 
+print(nuevo_usuario)
+
+producto = "Cuadernos"
+print(producto[-3:])
+print(producto[2:6])
+print(producto[:3])
---
+++ lista.py
@@ -0,0 +1,24 @@
+estudiantes = ['Ana', 'Luis', 'Carlos', 'Maria']
+
+
+print(estudiantes[0])
+
+estudiantes.append("Pedro")
+
+estudiantes.remove("Luis")
+
+print(estudiantes)
+
+frutas = ['manzana', 'manzana', 'banana', 'uva', 'manzana']
+
+print(frutas)
+
+print(frutas[2])
+
+frutas.append('peras')
+
+frutas.remove('manzana')
+frutas.remove('manzana')
+
+
+print(frutas)

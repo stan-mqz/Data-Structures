9549371c8fa661a1a33d4cb97e574760ee5a6605
stanmqz <moscarstanley37@gmail.com>
2026-08-31 11:53:13 -0600

    Add sets.py with initial set operations and language examples

---
+++ sets.py
@@ -0,0 +1,15 @@
+# lenguajes = Python - Java - JavaScript - PHP - Python - PHP
+
+set1 = set({1, 2, 3, 4, 5})
+print(set1)
+print(type(set1))
+
+set2 = {1, 2, 3, 4, 5}
+print(set2)
+print(type(set2))
+
+
+lenguajes = {"Python", "Java", "JavaScript", "PHP", "Python", "PHP"}
+
+
+print(lenguajes)
